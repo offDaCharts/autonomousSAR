51c876a5b213e279bfa17657848a4378f978560e
Quin <rqthames@yahoo.com>
2014-03-21 17:29:42 -0700

    testing image processing

--- readImageAndModify.py
+++ readImageAndModify.py
@@ -13,8 +13,8 @@ print len(image)
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # define range of orange color in HSV
-lower_orange = np.array([0,140,50])
-upper_orange = np.array([30,250,200])
+lower_orange = np.array([0,120,50])
+upper_orange = np.array([30,250,250])
 
 # Threshold the HSV image to get only blue colors
 mask = cv2.inRange(hsv, lower_orange, upper_orange)
