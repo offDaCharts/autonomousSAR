3de0e7cfaeea6e60c148cb818d154afd6a6a0873
Quin <rqthames@yahoo.com>
2014-03-21 17:40:06 -0700

    testing image processing

--- scan.py
+++ scan.py
@@ -60,7 +60,7 @@ while(scanNum < maxScans):
                 elif rho1 < 0 and rho2 < 0:
                     angle = 180 - abs(theta - phi)
                     complement = 180 - angle
-                print angle
+                #print angle
                 if (angle < upperAngle and angle > lowerAngle) or (complement > lowerAngle and complement < upperAngle):
                     starAngleCount += 1
 
