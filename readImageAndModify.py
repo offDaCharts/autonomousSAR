2ea458f5b3dffbefc8c2f4e6245767a059618a4c
Quin <rqthames@yahoo.com>
2014-04-15 09:22:42 -0700

    add pic

--- readImageAndModify.py
+++ readImageAndModify.py
@@ -28,8 +28,8 @@ print len(hsv[0])
 #s=180-190
 #v=230-250
 
-lower_orange = np.array([3,175,225])
-upper_orange = np.array([17,195,255])
+    lower_orange = np.array([0,155,210])
+    upper_orange = np.array([30,215,255])
 
 # Threshold the HSV image to get only blue colors
 mask = cv2.inRange(hsv, lower_orange, upper_orange)
