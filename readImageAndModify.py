feee8494c15a0b468cec48cef20bf84336498cb7
Quin <rqthames@yahoo.com>
2014-04-14 17:20:28 -0700

    adjusting threshholds

--- readImageAndModify.py
+++ readImageAndModify.py
@@ -16,8 +16,8 @@ hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 # lower_orange = np.array([0,120,50])
 #upper_orange = np.array([30,250,250])
 
-lower_orange = np.array([5,100,100])
-upper_orange = np.array([30,170,200])
+lower_orange = np.array([0,100,100])
+upper_orange = np.array([20,170,200])
 
 # Threshold the HSV image to get only blue colors
 mask = cv2.inRange(hsv, lower_orange, upper_orange)
