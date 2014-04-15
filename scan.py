c1c249fa3f7b9c869bb096aee7b492405d8cb12c
Quin <rqthames@yahoo.com>
2014-04-15 09:31:05 -0700

    filters

--- scan.py
+++ scan.py
@@ -38,8 +38,8 @@ while(scanNum < maxScans):
     #s=180-190
     #v=230-250
 
-    lower_orange = np.array([0,155,210])
-    upper_orange = np.array([30,215,255])
+    lower_orange = np.array([0,140,190])
+    upper_orange = np.array([30,230,255])
 
     # Threshold the HSV image to get only orange colors
     mask = cv2.inRange(hsv, lower_orange, upper_orange)
