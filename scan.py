062093f023f2496377a09d0ea18ccee4ecfd64e6
Quin <rqthames@yahoo.com>
2014-04-14 19:15:42 -0700

    adjusting threshholds

--- scan.py
+++ scan.py
@@ -38,8 +38,8 @@ while(scanNum < maxScans):
     #s=180-190
     #v=230-250
 
-    lower_orange = np.array([0,160,215])
-    upper_orange = np.array([25,210,255])
+    lower_orange = np.array([0,155,210])
+    upper_orange = np.array([30,215,255])
 
     # Threshold the HSV image to get only orange colors
     mask = cv2.inRange(hsv, lower_orange, upper_orange)
